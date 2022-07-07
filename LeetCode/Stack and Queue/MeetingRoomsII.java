25f823e1b802999a875ffff595cfa449828fb9d9
chenyu987 <lucas.yu.chen@gmail.com>
2022-07-07 18:00:56 -0400

    Update MeetingRoomsII.java

--- LeetCode/Stack and Queue/MeetingRoomsII.java
+++ LeetCode/Stack and Queue/MeetingRoomsII.java
@@ -1,3 +1,20 @@
+/*
+
+Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number of conference rooms required.
+
+ 
+
+Example 1:
+
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: 2
+Example 2:
+
+Input: intervals = [[7,10],[2,4]]
+Output: 1
+
+*/
+
 public class Solution {
     public int meetingRooms (Interval[] intervals) {
         if (intervals == null || intervals.length == 0) return 0;
@@ -22,6 +39,23 @@ public class Solution {
                 pq.offer(intervals[i].end);
                 pq.poll();
             }
+        }
+        return pq.size();
+    }
+}
+
+Wrote by me
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) return 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
+        pq.offer(intervals[0][1]);
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] >= pq.peek()) {
+                pq.poll();
+            }
+            pq.offer(intervals[i][1]);
         }
         return pq.size();
     }
