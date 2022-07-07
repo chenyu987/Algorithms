2f3acf8c169036e0bf9386da66d31a30d0dad368
chenyu987 <lucas.yu.chen@gmail.com>
2022-07-07 17:43:28 -0400

    Create 1244.DesignALeaderboard.java

---
+++ LeetCode/DataStructure/1244.DesignALeaderboard.java
@@ -0,0 +1,86 @@
+/*
+Design a Leaderboard class, which has 3 functions:
+
+addScore(playerId, score): Update the leaderboard by adding score to the given player's score. If there is no player with such id in the leaderboard, add him to the leaderboard with the given score.
+top(K): Return the score sum of the top K players.
+reset(playerId): Reset the score of the player with the given id to 0 (in other words erase it from the leaderboard). It is guaranteed that the player was added to the leaderboard before calling this function.
+Initially, the leaderboard is empty.
+
+ 
+
+Example 1:
+
+Input: 
+["Leaderboard","addScore","addScore","addScore","addScore","addScore","top","reset","reset","addScore","top"]
+[[],[1,73],[2,56],[3,39],[4,51],[5,4],[1],[1],[2],[2,51],[3]]
+Output: 
+[null,null,null,null,null,null,73,null,null,null,141]
+
+Explanation: 
+Leaderboard leaderboard = new Leaderboard ();
+leaderboard.addScore(1,73);   // leaderboard = [[1,73]];
+leaderboard.addScore(2,56);   // leaderboard = [[1,73],[2,56]];
+leaderboard.addScore(3,39);   // leaderboard = [[1,73],[2,56],[3,39]];
+leaderboard.addScore(4,51);   // leaderboard = [[1,73],[2,56],[3,39],[4,51]];
+leaderboard.addScore(5,4);    // leaderboard = [[1,73],[2,56],[3,39],[4,51],[5,4]];
+leaderboard.top(1);           // returns 73;
+leaderboard.reset(1);         // leaderboard = [[2,56],[3,39],[4,51],[5,4]];
+leaderboard.reset(2);         // leaderboard = [[3,39],[4,51],[5,4]];
+leaderboard.addScore(2,51);   // leaderboard = [[2,51],[3,39],[4,51],[5,4]];
+leaderboard.top(3);           // returns 141 = 51 + 51 + 39;
+*/
+
+class Leaderboard {
+
+    private HashMap<Integer, Integer> scores;
+    
+    public Leaderboard() {
+        this.scores = new HashMap<Integer, Integer>();
+    }
+    
+    public void addScore(int playerId, int score) {
+        
+        if (!this.scores.containsKey(playerId)) {
+            this.scores.put(playerId, 0);
+        }
+        
+        this.scores.put(playerId, this.scores.get(playerId) + score);
+    }
+    
+    public int top(int K) {
+        
+        // A min-heap in java containing entries of a hash map. Note that we have to provide
+        // a comparator of our own to make sure we get the ordering right of these objects.
+        PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+        
+        for (Map.Entry<Integer, Integer> entry : this.scores.entrySet()) {
+            heap.offer(entry);
+            if (heap.size() > K) {
+                heap.poll();
+            }
+        }
+        
+        int total = 0;
+        Iterator value = heap.iterator();
+        while (value.hasNext()) { 
+            total += value.next()).getValue();   
+        }
+        
+        return total;
+    }
+    
+    public void reset(int playerId) {
+        this.scores.put(playerId, 0);
+    }
+}
+
+
+
+
+/**
+ * Your Leaderboard object will be instantiated and called as such:
+ * Leaderboard obj = new Leaderboard();
+ * obj.addScore(playerId,score);
+ * int param_2 = obj.top(K);
+ * obj.reset(playerId);
+ */
